9903439fa9117ee5c2d6ece49e88fa01f3cc5d1f
Jake Grigsby <jcg6dn@virginia.edu>
2019-02-17 16:50:45 -0500

    add profile model, gitignore

--- interest_groups/models.py
+++ interest_groups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    registration_date = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to='static/images/profile_pictures/')
+    year_choices = [('1','1st Year'), ('2','2nd Year'),('3','3rd Year'),('4','4th Year'),('Grad', 'Graduate Student'),('Alumni','Alumni')]
+    year = models.CharField(choices=year_choices)
+    bio = models.TextField(max_length=500)
